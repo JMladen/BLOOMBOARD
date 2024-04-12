8e0a6f3d6cd6d47c97ef33e253b9d95e7504c824
Jack Mladen <jackmladen1@gmail.com>
2024-04-12 12:29:53 -0700

    refresh plant listings

--- server/index.js
+++ server/index.js
@@ -1,20 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const path = require("path");
-// const app = express();
-// const port = 3001;
-
-// app.use(cors());
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.get("/api/plants", (req, res) => {
-//   const plantsData = require("./data/plantsData.json");
-//   res.json(plantsData);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
